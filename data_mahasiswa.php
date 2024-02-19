dd1349a7bcd45c3503f5cfd97794dc02dc92aa68
samm098 <159239764+samm098@users.noreply.github.com>
2024-02-19 15:25:46 +0700

    Update data_mahasiswa.php
    Update Data Semester.

--- data_mahasiswa.php
+++ data_mahasiswa.php
@@ -1,6 +1,7 @@
 <?php
 echo "<h1>Data Mahasiswa</h1>";
 echo "Nim : 22110042 <br>";
-echo "Nama Lengkap : Samuel Ferinan Nababan";
-echo "Kelas : 22 MI B </h1>";
+echo "Nama Lengkap : Samuel Ferinan Nababan<br>";
+echo "Kelas : 22 MI B <br>";
+echo "Semester Genap";
 ?>
