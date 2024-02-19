e5ea3b8d15e88f39a1616033513008f63dbf94bd
samm098 <159239764+samm098@users.noreply.github.com>
2024-02-19 15:27:41 +0700

    Update data_mahasiswa.php
    Update Tahun Akademik

--- data_mahasiswa.php
+++ data_mahasiswa.php
@@ -2,5 +2,6 @@
 echo "<h1>Data Mahasiswa</h1>";
 echo "Nim : 22110042 <br>";
 echo "Nama Lengkap : Samuel Ferinan Nababan";
-echo "Kelas : 22 MI B </h1>";
+echo "Kelas : 22 MI B";
+echo "Tahun Akademik 2023/2024";
 ?>
